a6e3b468809192a273bf4e617f3504f25de2f755
x24138312 <x24138312@student.ncirl.ie>
2025-03-22 13:29:29 +0530

    Update settings.py

--- healthcare_system/settings.py
+++ healthcare_system/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 from decouple import config
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/dashboard/'
+LOGOUT_REDIRECT_URL = '/login/'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
